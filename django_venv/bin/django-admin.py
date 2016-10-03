9a661a0b49017869073b2fcffee57fab73f84e8c
East Agile <eastagile@East-Agile-5391.local>
2016-10-03 14:52:38 +0700

    Created project skeleton, ignored unnecessary files

---
+++ django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/eastagile/.pyenv/versions/2.7.11/envs/django_venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
